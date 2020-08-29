1f9524ea04bb1a332833400b5780b38f085a235d
ceifa <gabrinelson27@gmail.com>
2020-08-29 16:03:41 -0300

    Remove credential store

--- lua/includes/modules/xlib.lua
+++ lua/includes/modules/xlib.lua
@@ -11,10 +11,6 @@ _MODULES.xlib = true
 require "xloader"
 xloader("xlib", function(f) include(f) end)
 
-if SERVER then
-	require "credentialstore"
-end
-
 local extended = "xlib/xlib_extended.lua"
 if SERVER or file.Exists(extended, "LUA") then
 	include(extended)
